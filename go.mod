5785c22ea870e80d35e25e20b5315e08f5ac1c7b
ReyVega <A01114523@itesm.mx>
2021-06-05 20:22:36 -0500

    Adds SPACE_INVADERS.md and updates dependencies

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/faiface/pixel v0.10.0
 	github.com/oklog/ulid/v2 v2.0.2 // indirect
-	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
+	golang.org/x/image v0.0.0-20210504121937-7319ad40d33e
 )
--- lab.mk
+++ lab.mk
@@ -1,7 +1,14 @@
+deps:
+	go get github.com/faiface/pixel
+	go get github.com/faiface/pixel/imdraw
+	go get github.com/faiface/pixel/pixelgl
+	go get golang.org/x/image/colornames
+	go get github.com/oklog/ulid/v2
+
 build:
 	go build main.go
 
-test: build
+test: deps build
 	./main
 	
 clean:
